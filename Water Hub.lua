f2a1bde922e7a53dd9e46d912b129f0f64e5ccf7
penguinohio <119129827+penguinohio@users.noreply.github.com>
2023-02-19 19:19:30 +0100

    Create Water Hub.lua

---
+++ Water Hub.lua
@@ -0,0 +1,102 @@
+if game.PlaceId == 8750997647 then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow({Name = "Water Hub", HidePremium = false, IntroText = "Water Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+    getgenv().autowin1 = true
+    getgenv().autowin = true
+    getgenv().autorebirth = true
+
+    function autowin()
+            while getgenv().autowin == true do
+                task.wait()
+                game:GetService("ReplicatedStorage").Remotes.Tap:FireServer()
+
+        end
+    end
+
+    function autowin1()
+        while getgenv().autowin1 == true do
+            task.wait()
+            game:GetService("ReplicatedStorage").Remotes.Tap:FireServer()
+
+    end
+end
+
+    function autorebirth()
+        while getgenv().autorebirth == true do
+            task.wait(0.5)
+            local args = {
+                [1] = 11
+            }
+            
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Rebirth"):FireServer(unpack(args))            
+    end
+end
+
+    
+
+    local Tab = Window:MakeTab({
+        Name = "main",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    local Tabcredits = Window:MakeTab({
+        Name = "credits",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    Tab:AddToggle({
+        Name = "auto tap",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autowin = Value
+            autowin()
+        end    
+    })
+
+    Tab:AddToggle({
+        Name = "auto rebirth",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autorebirth = Value
+            autorebirth()
+        end    
+    })
+
+    Tab:AddToggle({
+        Name = "auto tap 2",
+        Default = false,
+        Callback = function(Value)
+            getgenv().autowin1 = Value
+            autowin1()
+        end    
+    })
+
+    local Sectioncredits = Tabcredits:AddSection({
+        Name = "by proohio"
+    })
+
+    Tabcredits:AddButton({
+        Name = "link for yt",
+        Callback = function()
+            setclipboard("https://www.youtube.com/channel/UCBM5Z9HC7kH-blWtA2Jgc-Q")
+          end    
+    })
+
+    local Sectioncredits = Tabcredits:AddSection({
+        Name = "by YAB Sleazy"
+    })
+
+    Tabcredits:AddButton({
+        Name = "link for yt",
+        Callback = function()
+            setclipboard("https://www.youtube.com/channel/UC9b3KgvigDzhaHKq6celzbQ")
+          end    
+    })
+
+    
+
+
+end
+    OrionLib:Init()
